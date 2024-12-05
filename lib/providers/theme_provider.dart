c5b05dc3ecfda00acaf36f1d12368818d77371b9
Aslam <aslam>
2024-12-05 22:44:28 +0530

    Update

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider with ChangeNotifier {
   bool _isDarkTheme = false;
@@ -7,8 +8,24 @@ class ThemeProvider with ChangeNotifier {
   bool get isDarkTheme => _isDarkTheme;
   ThemeData get theme => _isDarkTheme ? ThemeData.dark() : ThemeData.light();
 
+  ThemeProvider() {
+    _loadTheme();
+  }
+
   void toggleTheme() {
     _isDarkTheme = !_isDarkTheme;
+    _saveTheme(_isDarkTheme);
+    notifyListeners();
+  }
+
+  Future<void> _saveTheme(bool isDark) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDarkTheme', isDark);
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkTheme = prefs.getBool('isDarkTheme') ?? false;
     notifyListeners();
   }
 }
