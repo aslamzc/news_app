775d1713d3f0840dcc582e18d41e5274270195b9
Aslam <aslam>
2024-12-07 01:44:49 +0530

    Update

--- lib/providers/saved_news_provider.dart
+++ lib/providers/saved_news_provider.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:news/models/news.dart';
 
 class SavedNewsProvider with ChangeNotifier {
-
-
-  String _news = "fdfdfdfdfdf";
+  String _news = "test";
   String get news => _news;
 
   void setNews(String news) {
--- lib/utils/locator.dart
+++ lib/utils/locator.dart
@@ -1,8 +1,8 @@
 import 'package:get_it/get_it.dart';
-import 'package:news/providers/saved_news_provider.dart';
+// import 'package:news/providers/saved_news_provider.dart';
 
 final GetIt getIt = GetIt.instance;
 
 void setupLocator() {
-  getIt.registerLazySingleton(() => SavedNewsProvider());
+  // getIt.registerLazySingleton(() => SavedNewsProvider());
 }
